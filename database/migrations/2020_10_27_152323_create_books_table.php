4795c12a706ca72fde4201413cfac44bd78b338f
Alessandro <alessandro.boscato@gmail.com>
2020-10-27 16:31:07 +0100

    Migrate

---
+++ database/migrations/2020_10_27_152323_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('isbn', 13)->unique();
+            $table->string('title', 30);
+            $table->string('author', 50);
+            $table->string('genre', 30);
+            $table->string('edition', 50);
+            $table->smallInteger('pages');
+            $table->string('image');
+            $table->date('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
