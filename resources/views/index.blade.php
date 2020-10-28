f0285e2948ed872214bc3655864fd5ce4720a1d2
Alessandro <alessandro.boscato@gmail.com>
2020-10-28 17:45:02 +0100

    Fine progetto
    Risolvere il problema dell'auto-increment id

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Books</title>
 </head>
 <body>
   @foreach ($books as $book)
@@ -13,6 +13,7 @@
       <h3>{{$book->author}}</h3>
       <p>{{$book->edition}}</p>
       <img src="{{$book->image}}" alt="">
+      <span>{{$book->id}}</span>
     </div>
   @endforeach
 </body>
